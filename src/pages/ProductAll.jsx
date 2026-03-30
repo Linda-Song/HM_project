51d301ecf49b712751eedb9b5972c8b2226d87a6
Linda <poohzoa@gmail.com>
2026-03-30 11:30:55 +1100

    Fix: LoginPage case sensitivity issue

--- src/pages/ProductAll.jsx
+++ src/pages/ProductAll.jsx
@@ -10,7 +10,7 @@ const ProductAll = () => {
 
   const getProducts= async() => {
     let searchQuery = query.get("q") || "";
-    let url = `http://localhost:4000/products?q=${searchQuery}`;
+    let url = `https://my-json-server.typicode.com/Linda-Song/HM_project/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
